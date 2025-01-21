1d4003b56d00ac3255d0c9fe9a1f9e16ccd4d92a
Shanmukh srinadh <141711917+Shanmukhsrinadh@users.noreply.github.com>
2025-01-21 22:42:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function showLoginPrompt() {
    if (accountNumber && pin) {
        if (accountNumber === "123456789" && pin === "1234") {
            alert("Login successful!");
-           window.location.href = "accountDetails.html"; 
+           window.location.href = "./accountdetails.html"; 
        } else {
            alert("Invalid account number or PIN.");
        }

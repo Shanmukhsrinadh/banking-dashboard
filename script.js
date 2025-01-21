c0abaa990fed0f3b6ceacb36b14f8741457994ce
Shanmukhsrinadh <shimachalamshanmuk60297@gmail.com>
2025-01-21 22:31:15 +0530

    commit

--- script.js
+++ script.js
@@ -13,32 +13,19 @@
 
 function showLoginPrompt() {
    var modal = document.getElementById('loginModal');
-   modal.style.display = 'flex';  // Display the modal when the Login button is clicked
+   modal.style.display = 'flex'; 
  }
-//  window.onclick = function(event) {
-//    var modal = document.getElementById('loginModal');
-//    if (event.target === modal) {
-//      modal.style.display = 'none'; // Close modal when clicking outside of it
-//    }
-//  };
- 
  function handleLogin() {
-   // Get the values of the inputs
    let accountNumber = document.getElementById("accountNumber").value;
    let pin = document.getElementById("exampleInputPassword1").value;
-
-   // Check if both the account number and pin are provided
    if (accountNumber && pin) {
-       // Check if the account number and pin match the correct values
        if (accountNumber === "123456789" && pin === "1234") {
            alert("Login successful!");
            window.location.href = "accountDetails.html"; 
        } else {
-           // Show an alert if either the account number or pin is incorrect
            alert("Invalid account number or PIN.");
        }
    } else {
-       // Show an alert if either field is empty
        alert("Please enter both account number and PIN.");
    }
 }
